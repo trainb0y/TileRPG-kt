15e0899b88df43a894947ecd6807f79b8c489787
trainb0y1 <66213737+trainb0y1@users.noreply.github.com>
2022-01-25 19:34:30 -0700

    I need to grow more brain cells

--- lwjgl3/src/main/kotlin/io/github/trainb0y1/tilerpg/lwjgl3/Lwjgl3Launcher.kt
+++ lwjgl3/src/main/kotlin/io/github/trainb0y1/tilerpg/lwjgl3/Lwjgl3Launcher.kt
@@ -4,16 +4,10 @@ package io.github.trainb0y1.tilerpg.lwjgl3
 
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration
-import com.badlogic.gdx.tools.texturepacker.TexturePacker
 import io.github.trainb0y1.tilerpg.TileRPG
 
 /** Launches the desktop (LWJGL3) application. */
 fun main() {
-    val settings = TexturePacker.Settings()
-    settings.maxHeight = 512 // TODO: a more permanent solution for texture packing, it shouldn't be done every run
-    settings.maxWidth = 512
-
-    TexturePacker.process(settings, "../assets/","../assets/packed/","packed")
     Lwjgl3Application(TileRPG(), Lwjgl3ApplicationConfiguration().apply {
         setTitle("TileRPG")
         setWindowedMode(640, 480)
